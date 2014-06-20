4f53eff2107404d46f698cd75f8f28e0e9367713
Robert Honsby <rhonsby@gmail.com>
2014-06-20 11:02:50 -0700

    Add styling to results and update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@ ActiveRecord::Base.transaction do
   parsed_data.each do |truck_data|
     lat = truck_data["latitude"]
     long = truck_data["longitude"]
-    food_items = truck_data["fooditems"]
+    food_items = truck_data["fooditems"].gsub(':', ' -')
     raw_address_data = RestClient.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat},#{long}&key=#{ENV['GOOGLE_MAPS_API_KEY']}")
     address_data = JSON.parse(raw_address_data)["results"][0]
-    address = address_data && address_data["formatted_address"]
+    address = address_data["formatted_address"][0..-6]
 
     Truck.create(
       name: truck_data["applicant"],
